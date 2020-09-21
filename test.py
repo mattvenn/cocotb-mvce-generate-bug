68d0e6b8381e63fc336e75093dc1b17f35e374a5
matt venn <matt@mattvenn.net>
2020-09-21 12:16:22 +0200

    easier way of showing the example

--- test.py
+++ test.py
@@ -8,9 +8,12 @@ async def test(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.fork(clock.start())
     
-    import ipdb; ipdb.set_trace() 
-    # in the ipdb session type dut.genblk1, then press tab, then c to continue, the test will complete.
-    # If don't press tab, will fail with message: 'AttributeError: top contains no object named genblk1'
-    # tested with iverilog 10.3
+    # test fails with this error:
+    # AttributeError: top contains no object named genblk1
+    # change False to True below to make the test pass
+    if False:
+        for thing in dut:
+            thing._log.info("Found something: %s" % thing._fullname)
+
     await RisingEdge(dut.clk)
     print(dut.genblk1[0].sub_inst.clk.value)
