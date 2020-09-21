db3a58c35473c285b7e752b736cd5edee5c49852
matt venn <matt@mattvenn.net>
2020-09-21 12:23:20 +0200

    even simpler

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
-
 
 @cocotb.test()
 async def test(dut):
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.fork(clock.start())
-    
     # test fails with this error:
     # AttributeError: top contains no object named genblk1
     # change False to True below to make the test pass
@@ -15,5 +9,4 @@ async def test(dut):
         for thing in dut:
             pass
 
-    await RisingEdge(dut.clk)
     print(dut.genblk1[0].sub_inst.clk.value)
